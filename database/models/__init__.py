716aca3983d208acce7664ccde7ca337c851a85c
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-08-08 15:10:14 -0300

    Initialize ORM models in database package
    Added ORM models for various entities in the database.

---
+++ database/models/__init__.py
@@ -0,0 +1,43 @@
+"""
+Modelos ORM do banco de dados
+Cada arquivo representa uma tabela/entidade
+"""
+
+from database.models.user import User
+from database.models.admin import Admin, AdminPermission
+from database.models.category import Category
+from database.models.product import Product, ProductMedia
+from database.models.stock_item import StockItem
+from database.models.order import Order, OrderItem
+from database.models.payment import Payment, PixTransaction
+from database.models.affiliate import Affiliate, AffiliateCommission
+from database.models.button import Button
+from database.models.message_template import MessageTemplate
+from database.models.system_setting import SystemSetting
+from database.models.notification import Notification
+from database.models.media import Media
+from database.models.audit_log import AuditLog
+from database.models.user_session import UserSession
+
+__all__ = [
+    "User",
+    "Admin",
+    "AdminPermission",
+    "Category",
+    "Product",
+    "ProductMedia",
+    "StockItem",
+    "Order",
+    "OrderItem",
+    "Payment",
+    "PixTransaction",
+    "Affiliate",
+    "AffiliateCommission",
+    "Button",
+    "MessageTemplate",
+    "SystemSetting",
+    "Notification",
+    "Media",
+    "AuditLog",
+    "UserSession",
+]
