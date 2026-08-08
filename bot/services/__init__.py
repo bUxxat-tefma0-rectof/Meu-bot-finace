2f06c165ce599b822dbe650229345fc11096367d
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-08-08 16:05:31 -0300

    Translate package docstring to Portuguese
    Updated package docstring to Portuguese and added imports.

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,3 +1,12 @@
 """
-Bot services package
+Serviços do Bot
+Serviços auxiliares para navegação e sessão
 """
+
+from bot.services.navigation_service import NavigationService
+from bot.services.session_service import SessionService
+
+__all__ = [
+    "NavigationService",
+    "SessionService",
+]
